52332a10bc6518dc11b43d07a55a900f0b75b809
mayankst13 <stmayank92@gmail.com>
2025-12-11 11:18:03 +0530

    update

--- String/Basic/strlength.java
+++ String/Basic/strlength.java
@@ -1,5 +1,10 @@
 package String.Basic;
-
+import java.util.Scanner;
 public class strlength {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String str = sc.nextLine();
+        System.out.println("Length of String is: " + str.length());
+    }
 }
